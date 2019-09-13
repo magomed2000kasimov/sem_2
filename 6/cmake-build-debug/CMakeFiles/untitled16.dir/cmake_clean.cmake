fc95f1cbc697d32380d432aefd560b664960f5a6
magomed kasimov <50713505+magomed2000kasimov@users.noreply.github.com>
2019-09-13 10:06:14 +0300

    Add files via upload

---
+++ 6/cmake-build-debug/CMakeFiles/untitled16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled16.dir/main.c.obj"
+  "untitled16.pdb"
+  "untitled16.exe"
+  "untitled16.exe.manifest"
+  "libuntitled16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
